0fee6eff6abd117fd0a9c8582edeccb96f0ceb2c
Evgeny Tarasov <tarasov.e.a@gmail.com>
2020-01-23 10:51:33 -0500

    test

--- deployments/job_samples_data_kickoff.sh
+++
@@ -1,4 +0,0 @@
-export COMMIT_SHA=`git rev-parse HEAD`
-sed -e "s|__VERSION__|$COMMIT_SHA|g" deployments/job_samples_data_template.yaml | tee deployments/job_samples_data.yaml
-kubectl apply -f deployments/job_samples_data.yaml
-rm deployments/job_samples_data.yaml
