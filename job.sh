7951776f342823ddbd9a35f98f9cb86684d4716e
Evgeny Tarasov <tarasov.e.a@gmail.com>
2020-02-27 15:38:24 -0500

    test

---
+++ job.sh
@@ -0,0 +1,33 @@
+export COMMIT_SHA=`git rev-parse HEAD`
+
+# download
+export JOB_NAME=download
+export NUM_JOBS=20
+export PARALLELISM=20
+export SCRIPT=download.py
+
+# transpose
+export JOB_NAME=transpose
+export NUM_JOBS=75
+export PARALLELISM=1
+export SCRIPT=transpose.py
+
+# concat daily chunks
+export JOB_NAME=concat-daily-chunks
+export NUM_JOBS=75
+export PARALLELISM=1
+export SCRIPT=concat-daily-chunks.py
+
+mkdir tmp
+
+for (( INDEX=0; INDEX<NUM_JOBS; INDEX++ ))
+do
+	sed -e "s|__VERSION__|$COMMIT_SHA|g" -e "s|__INDEX__|$INDEX|g" -e "s|__NUM_JOBS__|$NUM_JOBS|g" -e "s|__JOB_NAME__|$JOB_NAME|g" -e "s|__PARALLELISM__|$PARALLELISM|g" job.yaml | tee tmp/job_$INDEX.yaml
+done
+
+for (( INDEX=0; INDEX<NUM_JOBS; INDEX++ ))
+do
+	kubectl apply -f tmp/job_$INDEX.yaml
+done
+
+rm -r tmp
