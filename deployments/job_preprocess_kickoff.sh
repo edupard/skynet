b75d356497e9b4ec7ab61e3a00c8588368f6f2b0
Evgeny Tarasov <tarasov.e.a@gmail.com>
2020-02-27 15:55:15 -0500

    test

--- deployments/job_preprocess_kickoff.sh
+++
@@ -1,4 +0,0 @@
-export COMMIT_SHA=`git rev-parse HEAD`
-sed -e "s|__VERSION__|$COMMIT_SHA|g" deployments/job_preprocess_template.yaml | tee deployments/job_preprocess.yaml
-kubectl apply -f deployments/job_preprocess.yaml
-rm deployments/job_preprocess.yaml
--- deployments/job_samples_daily_tickers_kickoff.sh
+++
@@ -1,4 +0,0 @@
-export COMMIT_SHA=`git rev-parse HEAD`
-sed -e "s|__VERSION__|$COMMIT_SHA|g" deployments/job_samples_daily_tickers_template.yaml | tee deployments/job_samples_daily_tickers.yaml
-kubectl apply -f deployments/job_samples_daily_tickers.yaml
-rm deployments/job_samples_daily_tickers.yaml
