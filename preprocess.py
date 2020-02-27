f814a61668d3a06a1737b19b3a6afdb8cea8fa14
Evgeny Tarasov <tarasov.e.a@gmail.com>
2020-02-27 17:53:39 -0500

    test

--- preprocess.py
+++ preprocess.py
@@ -1,76 +1,91 @@
-import tempfile
-import os
-from google.cloud import storage
+import utils.messaging as messaging
+from abstractions.log import log
 import numpy as np
-import math
-import traceback
-import sys
+import datetime
+from utils.utils import arr_rema, i_to_date, roll_arr_fwd
+import utils.gcs as gcs
+import pandas as pd
 
-#https://www.youtube.com/watch?v=ffDLG7Vt6JE&t=18s
-
-ANN_FACTOR = 252.75
-
-def ema(prev_ema, val, gamma):
-    return val * (1 - gamma) + gamma * prev_ema
+gcs_client = gcs.GcsClient()
 
 
-def arr_ema(arr, gamma):
-    def _ema(prev_ema, val):
-        return ema(prev_ema, val, gamma)
-
-    v_ema = np.frompyfunc(_ema, 2, 1)
-
-    return v_ema.accumulate(arr, dtype=np.object).astype(np.float)
-
-
-def arr_rema(arr, gamma):
-    intermediate = arr_ema(arr[::-1], gamma)
-    return intermediate[::-1]
+def get_idx_arr(i_dates: np.ndarray, min_date: datetime.date):
+    indexes = np.zeros_like(i_dates)
+    idx = 0
+    for i in i_dates:
+        indexes[idx] = (i_to_date(i_dates[idx]) - min_date).days
+        idx = idx + 1
+    return indexes
 
 
-def process_ticker(ticker):
-    # get csv file
-    client = storage.Client()
-    bucket = client.get_bucket('skynet-1984-data')
-    blob = bucket.get_blob(f'{ticker}.csv')
-    if blob is None:
-        return
-
-    fd, tmp_file_name = tempfile.mkstemp()
-    os.close(fd)
-    blob.download_to_filename(tmp_file_name)
-
-    # read file
-    data = np.genfromtxt(tmp_file_name, delimiter=',', skip_header=1)
+def get_raw_data(total_points, idx_arr, df):
+    # ['v', 't', 'a_o', 'a_h', 'a_l', 'a_c']
+    raw = np.zeros((total_points, 7))
 
     # ['date', 'o', 'h', 'l', 'c', 'v', 'a_o', 'a_h', 'a_l', 'a_c', 'a_v', 'div', 'split']
+    raw[idx_arr, 0] = df.v.values
+    raw[idx_arr, 1] = (df.h.values + df.l.values + df.c.values) / 3
+    raw[idx_arr, 2] = df.a_o.values
+    raw[idx_arr, 3] = df.a_h.values
+    raw[idx_arr, 4] = df.a_l.values
+    raw[idx_arr, 5] = df.a_c.values
+    raw[idx_arr, 6] = df.c.values
 
-    o = data[:, 1]
-    h = data[:, 2]
-    l = data[:, 3]
-    c = data[:, 4]
-    v = data[:, 5]
+    # now we apply hacks
+    zero_volume_mask = raw[:, 0] == 0
+    # no trading activity interpreted as very small quantity
+    raw[zero_volume_mask, 0] = 100
 
-    t = (h + l + c) / 3
-    gv = v * t
+    # roll closing price
+    raw[:, 5] = roll_arr_fwd(raw[:, 5])
+    raw[:, 6] = roll_arr_fwd(raw[:, 6])
 
-    gv_ema = arr_ema(gv, 0.95)
+    # copy closing price if other prices is missing
+    zero_prices_mask = raw[:, 1] == 0
+    raw[zero_prices_mask, 1] = raw[zero_prices_mask, 6]
+    zero_prices_mask = raw[:, 2] == 0
+    raw[zero_prices_mask, 2] = raw[zero_prices_mask, 5]
+    zero_prices_mask = raw[:, 3] == 0
+    raw[zero_prices_mask, 3] = raw[zero_prices_mask, 5]
+    zero_prices_mask = raw[:, 4] == 0
+    raw[zero_prices_mask, 4] = raw[zero_prices_mask, 5]
 
-    a_o = data[:, 6]
-    a_h = data[:, 7]
-    a_l = data[:, 8]
-    a_c = data[:, 9]
-    a_v = data[:, 10]
+    return raw[:,:6]
+
+
+def get_input_from_raw(raw):
+    gv = raw[:, 0] * raw[:, 1]
+    a_o = raw[:, 2]
+    a_h = raw[:, 3]
+    a_l = raw[:, 4]
+    a_c = raw[:, 5]
+
     a_c_0 = a_c[0]
     a_c_t_min_1 = np.roll(a_c, 1, axis=0)
     a_c_t_min_1[0] = a_c_0
+
+    gv_0 = gv[0]
+    gv_t_min_1 = np.roll(gv, 1, axis=0)
+    gv_t_min_1[0] = gv_0
 
     lr_o = np.log(a_o / a_c_t_min_1)
     lr_h = np.log(a_h / a_c_t_min_1)
     lr_l = np.log(a_l / a_c_t_min_1)
     lr_c = np.log(a_c / a_c_t_min_1)
-    v_pct = gv / gv_ema - 1
+    lr_gv = np.log(gv / gv_t_min_1)
 
+    result = np.stack([lr_o, lr_h, lr_l, lr_c, lr_gv], axis=1)
+    return result
+
+
+def get_output(raw, gamma):
+    # gv, t, a_o, a_h, a_l, a_c
+    a_c = raw[:, 5]
+    a_c_0 = a_c[0]
+    a_c_t_min_1 = np.roll(a_c, 1, axis=0)
+    a_c_t_min_1[0] = a_c_0
+
+    lr_c = np.log(a_c / a_c_t_min_1)
     sigma = lr_c * lr_c
 
     lr_c_t__plus_1 = np.roll(lr_c, -1, axis=0)
@@ -78,59 +93,107 @@ def process_ticker(ticker):
     sigma_t_plus_1 = np.roll(sigma, -1, axis=0)
     sigma_t_plus_1[-1] = 0
 
-    lr_ema_70 = arr_rema(lr_c_t__plus_1, 0.70)
-    sigma_ema_70 = arr_rema(sigma_t_plus_1, 0.70)
-    stddev_ema_70 = np.sqrt(sigma_ema_70)
-    lr_ema_70_ann = ANN_FACTOR * lr_ema_70
-    stddev_ema_70_ann = math.sqrt(ANN_FACTOR) * stddev_ema_70
+    lr_rema = arr_rema(lr_c_t__plus_1, gamma)
+    sigma_rema = arr_rema(sigma_t_plus_1, gamma)
+    stddev_rema = np.sqrt(sigma_rema)
+    lr_rema_ann = lr_rema
+    stddev_rema_ann = stddev_rema
 
-    lr_ema_82 = arr_rema(lr_c_t__plus_1, 0.82)
-    sigma_ema_82 = arr_rema(sigma_t_plus_1, 0.82)
-    stddev_ema_82 = np.sqrt(sigma_ema_82)
-    lr_ema_82_ann = ANN_FACTOR * lr_ema_82
-    stddev_ema_82_ann = math.sqrt(ANN_FACTOR) * stddev_ema_82
-
-    lr_ema_91 = arr_rema(lr_c_t__plus_1, 0.91)
-    sigma_ema_91 = arr_rema(sigma_t_plus_1, 0.91)
-    stddev_ema_91 = np.sqrt(sigma_ema_91)
-    lr_ema_91_ann = ANN_FACTOR * lr_ema_91
-    stddev_ema_91_ann = math.sqrt(ANN_FACTOR) * stddev_ema_91
-
-    lr_ema_95 = arr_rema(lr_c_t__plus_1, 0.95)
-    sigma_ema_95 = arr_rema(sigma_t_plus_1, 0.95)
-    stddev_ema_95 = np.sqrt(sigma_ema_95)
-    lr_ema_95_ann = ANN_FACTOR * lr_ema_95
-    stddev_ema_95_ann = math.sqrt(ANN_FACTOR) * stddev_ema_95
-
-    result = np.concatenate([data,
-                             np.reshape(lr_o, [-1, 1]),
-                             np.reshape(lr_h, [-1, 1]),
-                             np.reshape(lr_l, [-1, 1]),
-                             np.reshape(lr_c, [-1, 1]),
-                             np.reshape(v_pct, [-1, 1]),
-                             np.reshape(lr_ema_70_ann, [-1, 1]),
-                             np.reshape(stddev_ema_70_ann, [-1, 1]),
-                             np.reshape(lr_ema_82_ann, [-1, 1]),
-                             np.reshape(stddev_ema_82_ann, [-1, 1]),
-                             np.reshape(lr_ema_91_ann, [-1, 1]),
-                             np.reshape(stddev_ema_91_ann, [-1, 1]),
-                             np.reshape(lr_ema_95_ann, [-1, 1]),
-                             np.reshape(stddev_ema_95_ann, [-1, 1])
-                             ], axis=1)
-    np.savetxt(tmp_file_name,
-               result,
-               header='date,o,h,l,c,v,a_o,a_h,a_l,a_c,a_v,div,split,lr_o,lr_h,lr_l,lr_c,v_pct,lr_70,std_70,lr_82,std_82,lr_91,std_91,lr_95,std_95')
-
-    bucket = client.get_bucket('skynet-1984-data-enriched')
-    blob = bucket.blob(f'{ticker}.csv')
-    blob.upload_from_filename(tmp_file_name)
-
-    os.remove(tmp_file_name)
+    return np.stack([lr_rema_ann,
+                     stddev_rema_ann], axis=1)
 
 
-def preprocess_tickers(tickers):
-    for ticker in tickers:
-        try:
-            process_ticker(ticker)
-        except:
-            traceback.print_exc(file=sys.stdout)
+def preprocess(ticker, df, spy_df):
+    # get dates
+    i_spy_dates = spy_df.date.values.astype(np.int)
+    i_dates = df.date.values.astype(np.int)
+
+    # eq
+    i_min_date = i_dates[0]
+    i_max_date = i_dates[-1]
+    min_date = i_to_date(i_min_date)
+    max_date = i_to_date(i_max_date)
+    # snp
+    i_min_spy_date = i_spy_dates[0]
+    i_max_spy_date = i_spy_dates[-1]
+    min_spy_date = i_to_date(i_min_spy_date)
+    max_spy_date = i_to_date(i_max_spy_date)
+
+    # global indexes relatives to spy begin
+    spy_idx_arr = get_idx_arr(i_spy_dates, min_spy_date)
+    idx_arr = get_idx_arr(i_dates, min_spy_date)
+
+    total_points = (max_spy_date - min_spy_date).days + 1
+    spy_raw = get_raw_data(total_points, spy_idx_arr, spy_df)
+    raw = get_raw_data(total_points, idx_arr, df)
+
+    # now leave only active trading days: ie SPY is traded
+    spy_raw = spy_raw[spy_idx_arr, :]
+    raw = raw[spy_idx_arr, :]
+
+    spy_input = get_input_from_raw(spy_raw)
+
+    # spy dates less than i_max_date beacuse last day volume is usually 0 - price is fixed
+    dates_mask = (i_spy_dates >= i_min_date) & (i_spy_dates < i_max_date)
+
+    spy_input = spy_input[dates_mask, :]
+    spy_dates = i_spy_dates[dates_mask]
+
+    raw = raw[dates_mask, :]
+    raw_input = get_input_from_raw(raw)
+    raw_output = get_output(raw, 0.0)
+    dict = {
+        'date': spy_dates,
+        'spy_lr_o': spy_input[:, 0],
+        'spy_lr_h': spy_input[:, 1],
+        'spy_lr_l': spy_input[:, 2],
+        'spy_lr_c': spy_input[:, 3],
+        'spy_lr_gv': spy_input[:, 4],
+        'lr_o': raw_input[:, 0],
+        'lr_h': raw_input[:, 1],
+        'lr_l': raw_input[:, 2],
+        'lr_c': raw_input[:, 3],
+        'lr_gv': raw_input[:, 4],
+        'p_lr': raw_output[:, 0]
+    }
+    df = pd.DataFrame(dict)
+    lfp = f'/tmp/preprocessed_{ticker}.csv'
+    rfp = f'tiingo/preprocessed/{ticker}.csv'
+    df.to_csv(lfp, index=False)
+    gcs_client.save(lfp, rfp)
+
+
+gcs_client.get("tiingo/stocks/SPY.csv", '/tmp/SPY.csv')
+gcs_client.get("tiingo/tickers.csv", '/tmp/tickers.csv')
+
+spy_df = pd.read_csv('/tmp/SPY.csv')
+tickers_df = pd.read_csv('/tmp/tickers.csv')
+
+subscriber = messaging.Subscriber()
+
+while True:
+    messages, to_ack = subscriber.pull_messages('tickers', 1)
+    if len(messages) == 0:
+        break
+    for ticker in messages:
+        ticker_info = tickers_df.query(f'ticker == "{ticker}"')
+        if ticker_info.shape[0] == 0:
+            continue
+        sStartDate = ticker_info.iloc[0].startDate
+        sEndDate = ticker_info.iloc[0].endDate
+        if not isinstance(sStartDate, str) or not isinstance(sEndDate, str):
+            continue
+        start_date = datetime.datetime.strptime(sStartDate, "%Y-%m-%d")
+        end_date = datetime.datetime.strptime(sEndDate, "%Y-%m-%d")
+        if (end_date - start_date).days <= 65:
+            continue
+
+        rfp = f'tiingo/stocks/{ticker}.csv'
+        lfp = f'/tmp/{ticker}.csv'
+        gcs_client.get(rfp, lfp)
+        df = pd.read_csv(lfp)
+
+        log(f"Preprocessing {ticker} stock data")
+        preprocess(ticker, df, spy_df)
+
+    subscriber.ack('tickers', to_ack)
