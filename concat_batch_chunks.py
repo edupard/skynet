00e641407f95ccf5d625d5c4112fe63ff35ca24e
Evgeny Tarasov <tarasov.e.a@gmail.com>
2020-02-29 15:17:55 -0500

    test

---
+++ concat_batch_chunks.py
@@ -0,0 +1,27 @@
+import utils.gcs as gcs
+import numpy as np
+import sys
+
+worker_idx = int(sys.argv[1])
+num_workers = int(sys.argv[2])
+
+batch_id = worker_idx
+
+gcs_client = gcs.GcsClient()
+
+FROM = 1993
+TO = 2008
+
+chunks = []
+for i in range(75):
+    rfp = f'tiingo/batch_chunks/{FROM}_{TO}/{batch_id}_{i}.npy'
+    lfp = f'/tmp/{batch_id}_{i}.npy'
+    if not gcs_client.get(rfp, lfp):
+        continue
+    chunks.append(np.load(lfp))
+
+batch = np.concatenate(chunks, axis=0)
+lfp = f'/tmp/{batch_id}.npy'
+rfp = f'tiingo/batches/{FROM}_{TO}/{batch_id}.npy'
+np.save(lfp, batch)
+gcs_client.save(lfp, rfp)
--- job.sh
+++ job.sh
@@ -38,14 +38,14 @@ export SCRIPT=create_batch_chunks.py
 
 mkdir tmp
 
-#for INDEX in {52,60}
-for (( INDEX=0; INDEX<NUM_JOBS; INDEX++ ))
+for INDEX in {69,1,17,4,71,8}
+#for (( INDEX=0; INDEX<NUM_JOBS; INDEX++ ))
 do
 	sed -e "s|__VERSION__|$COMMIT_SHA|g" -e "s|__INDEX__|$INDEX|g" -e "s|__NUM_JOBS__|$NUM_JOBS|g" -e "s|__JOB_NAME__|$JOB_NAME|g" -e "s|__PARALLELISM__|$PARALLELISM|g" -e "s|__SCRIPT__|$SCRIPT|g" job.yaml | tee tmp/job_$INDEX.yaml
 done
 
-#for INDEX in {52,60}
-for (( INDEX=0; INDEX<NUM_JOBS; INDEX++ ))
+for INDEX in {69,1,17,4,71,8}
+#for (( INDEX=0; INDEX<NUM_JOBS; INDEX++ ))
 do
 	kubectl apply -f tmp/job_$INDEX.yaml
 done
